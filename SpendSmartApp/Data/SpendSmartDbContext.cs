1d1fd221c749dfa3cc68137f85d2341f7f845c8b
legiomaria <chikakool2010@gmail.com>
2024-09-03 12:34:30 +0200

    Add project files.

---
+++ SpendSmartApp/Data/SpendSmartDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpendSmartApp.Models;
+
+namespace SpendSmartApp.Data
+{
+    public class SpendSmartDbContext : DbContext
+    {
+        public DbSet<Expense> Expenses { get; set; }
+
+        public SpendSmartDbContext(DbContextOptions<SpendSmartDbContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
---
+++ SpendSmartApp/Models/Expense.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmartApp.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description { get; set; }
+    }
+}
